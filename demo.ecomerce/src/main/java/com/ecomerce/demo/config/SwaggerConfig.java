6bbe78ab6ef685ed530fedf8675d3fe5b310daef
carjiso <cjimenez@indigitalsolutions.com>
2025-02-05 23:02:54 -0500

    fix docs

---
+++ demo.ecomerce/src/main/java/com/ecomerce/demo/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.ecomerce.demo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Ecommerce API")
+                        .version("1.0")
+                        .description("Documentación de la API del Ecommerce"));
+    }
+}
